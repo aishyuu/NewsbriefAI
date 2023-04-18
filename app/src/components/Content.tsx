ac554e3cdfd62e3ca66cd54ec890ac81d64c671c
aishyuu <ivanmatias1331@gmail.com>
2023-04-17 20:11:38 -0700

    Created basic UI for putting in link (before adding functionality)

--- app/src/components/Content.tsx
+++ app/src/components/Content.tsx
@@ -4,9 +4,14 @@ export default function Content() {
             <div className="p-6 mt-4 text-3xl text-center">
                 <span>Get summaries of news articles through the power of AI</span>
             </div>
-            <div className="mx-auto">
-                <span className="block">Article URL</span>
-                <input type="text" name="" id="" />
+            <div className="mx-auto w-4/6 flex flex-col items-center">
+                <span className="italic">Article URL</span>
+                <input type="text" name="" id="" 
+                    className="border w-full xl:w-3/5"
+                />
+                <button className="mt-8 px-5 py-2 border bg-blue-400 text-lg dark:border-none dark:bg-gray-700">
+                    Summarize
+                </button>
             </div>
         </div>
     )
